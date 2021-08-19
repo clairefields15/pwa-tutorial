d6c17ec63633c563ef430d5bf78396fe718f1b7d
Claire Fields <clairefields15@gmail.com>
2021-08-19 13:39:18 -0600

    Create controlled form to get weather based on zip or town

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchWeather } from '../../api/apiCalls';
+import './App.css';
 
 export const App = () => {
-  return <h1>Hi PWA</h1>;
+  const [town, setTown] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const handleClick = async e => {
+    e.preventDefault();
+    let data = await fetchWeather(town);
+    setWeather(data);
+  };
+
+  return (
+    <main>
+      <header>
+        <h1>Weatherly</h1>
+      </header>
+      <section>
+        <form>
+          <label htmlFor='town'>Enter a town or zip to get weather</label>
+          <input
+            id='town'
+            type='text'
+            placeholder='Town or zip'
+            onChange={e => setTown(e.target.value)}
+            value={town}
+          />
+          <button onClick={e => handleClick(e)}>Get Weather</button>
+        </form>
+      </section>
+    </main>
+  );
 };
